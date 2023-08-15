63ee0116d0acc0fddf8aa8431113bcc588fd406a
rosstewart <129696920+rosstewart@users.noreply.github.com>
2023-08-15 01:15:50 -0400

    Update PacmanTester.java

--- PacmanTester.java
+++ PacmanTester.java
@@ -2,13 +2,13 @@ public class PacmanTester {
 
     public static void main(String[] arg)
     {
-    	Pacman pacman = new Pacman("src/lab12/mazes/bigMaze.txt","src/lab12/output.txt");
-    	Pacman pacmanBFS = new Pacman("src/lab12/mazes/bigMazeBFSSol.txt","src/lab12/dontwrite.txt");
-    	Pacman pacmanDFS = new Pacman("src/lab12/mazes/bigMazeDFSSol.txt","src/lab12/dontwrite.txt");
+    	Pacman pacman = new Pacman("mazes/bigMaze.txt","output.txt");
+    	Pacman pacmanBFS = new Pacman("mazes/bigMazeBFSSol.txt","dontwrite.txt");
+    	Pacman pacmanDFS = new Pacman("mazes/bigMazeDFSSol.txt","dontwrite.txt");
     	pacman.solveBFS();
     	pacman.writeOutput();
     	System.out.println(pacman.toString().equals(pacmanBFS.toString()));
-    	Pacman pacman2 = new Pacman("src/lab12/mazes/bigMaze.txt","src/lab12/output2.txt");
+    	Pacman pacman2 = new Pacman("mazes/bigMaze.txt","output2.txt");
     	pacman2.solveDFS();
     	pacman2.writeOutput();
     	System.out.println(pacman2.toString().equals(pacmanDFS.toString()));
